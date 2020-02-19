02d2e45b464637d1d3136e53d6e3589f9fe8e40e
lsw1164 <qkqkfo@gmail.com>
2020-02-20 00:14:21 +0900

    Use bable-node & nodemon

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-//require : get something from node module
-const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
 
-function handleHome(req, res) {
-  res.send("Hello from home");
-}
+const handleHome = (req, res) => res.send("Hello from home");
 
-function handleProfile(req, res) {
-  res.send("You are on my profile");
-}
+const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
