de42aa38b1f1a38ab8cf11b648fdae0110ebc91c
lsw1164 <qkqkfo@gmail.com>
2020-02-21 01:28:03 +0900

    Add first middlewares

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
-const PORT = 4000;
+const PORT = 8000;
 
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
 
-const handleHome = (req, res) => res.send("Hello from home");
+const handleHome = (req, res) => res.send("Hello from home!!!");
 
-const handleProfile = (req, res) => res.send("You are on my profile");
+const handleProfile = (req, res) => res.send("You are on my profile!");
 
+//middleware
+const betweenHome = (req, res, next) => {
+  console.log("Between");
+  next();
+}
+
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(morgan("combined"));
+app.use(betweenHome);
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
