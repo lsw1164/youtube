a186f7d0d6dea8a6ff6baa4ca80d59ffdd6f360b
lsw1164 <qkqkfo@gmail.com>
2020-02-27 20:39:16 +0900

    Use Mixin

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
         title:'Video awesome',
         description:'This is something I love',
         views:24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id:"121212",
             name:"lsw",
@@ -17,7 +17,7 @@ export const videos = [
         title:'Video Nice',
         description:'This is something I love',
         views:24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id:"121212",
             name:"lsw",
@@ -30,7 +30,7 @@ export const videos = [
         title:'Video Perfect',
         description:'This is something I love',
         views:24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id:"121212",
             name:"lsw",
@@ -43,7 +43,7 @@ export const videos = [
         title:'Video Good',
         description:'This is something I love',
         views:24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id:"121212",
             name:"lsw",
