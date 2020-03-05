e1839db59880ee076d350c3c527e39e6c543c0cb
lsw1164 <qkqkfo@gmail.com>
2020-03-05 22:56:57 +0900

    Add features - edit & delete a video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "../routes";
-import { videoDetail, editVideo, deleteVideo, getUpload, postUpload } from "../controllers/videoControllers";
+import { videoDetail, deleteVideo, getUpload, postUpload, getEditVideo, postEditVideo } from "../controllers/videoControllers";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
@@ -10,7 +10,10 @@ videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+videoRouter.get(routes.editVideo(), getEditVideo); 
+videoRouter.post(routes.editVideo(), postEditVideo); 
+
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
