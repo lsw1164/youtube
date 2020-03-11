4a6eb50e7d48bab844317bf7e0a6fe64c0c5cd6c
lsw1164 <qkqkfo@gmail.com>
2020-03-11 22:43:50 +0900

    Local Authentication with Passport

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,17 +1,28 @@
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
-export const postJoin = (req, res) => {
-  console.log(req.body);
+export const postJoin = async (req, res) => {
   const {
-    body: { password, password2 }
+    body: { name, email, password, password2 }
   } = req;
   if (password !== password2) {
     res.status(400);
     res.render("join", { pageTitle: "Join" });
   } else {
+    console.log(name);
+    console.log(email);
+    try {
+      const user = await User.create({
+        name,
+        email
+      });
+      await User.register(user, password);
+    } catch (error) {
+      console.log(error);
+    }
     //TODO: Register User
     //TODO: Log user
     res.redirect(routes.home);
