4edc970e174b83890948c59eeca1c42161c6961d
lsw1164 <qkqkfo@gmail.com>
2020-03-15 23:56:32 +0900

    Add Creator to Video.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
 const CommentSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        required: "Title is required"
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  text: {
+    type: String,
+    required: "Title is required"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
 });
 
 const model = mongoose.model("Coment", CommentSchema);
