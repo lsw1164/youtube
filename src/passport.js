0c7b9c898454529fee859fbd2f1f662ef87f805f
swlee <swlee@bitmango.com>
2020-03-28 03:19:45 +0900

    Change github authorize

--- src/passport.js
+++ src/passport.js
@@ -10,12 +10,15 @@ import routes from "./routes";
 
 passport.use(User.createStrategy());
 
+const PORT = process.env.PORT || 4000;
+const HOME_URL_PATH = process.env.PRODUCTION ? "https://gentle-forest-92388.herokuapp.com" : `http://localhost:${PORT}`;
+
 passport.use(
   new GithubStrategy(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`
+      callbackURL: `${HOME_URL_PATH}${routes.githubCallback}`
     },
     githubLoginCallback
   )
@@ -40,7 +43,7 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `http://localhost:4000${routes.facebookCallback}`,
+      callbackURL: `${HOME_URL_PATH}${routes.facebookCallback}`,
       profileFields: ["id", "displayName", "photos", "email"],
       scope: ["public_profile", "email"]
     },
